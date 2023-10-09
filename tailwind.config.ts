27a567a451688a56488de3d04d6389a6c5354456
Alek Novkovski <alek@greatcode2.com>
2023-10-09 21:50:06 +0200

    Tailwind config: define animation, xs screen

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -53,6 +53,23 @@ const config: Config = {
         "auth-dark": "url('/assets/images/auth-dark.png')",
         "auth-light": "url('/assets/images/auth-light.png')",
       },
+      screens: {
+        xs: "420px",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
     },
   },
   plugins: [],
