b5d314ae3e9b84791f31154750c40fa04608833a
Alek Novkovski <alek@greatcode2.com>
2023-11-02 17:26:14 +0100

    Define metadata icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,10 @@ const spaceGrotesk = Space_Grotesk({
 
 export const metadata: Metadata = {
   title: 'DevFlow',
-  description: 'A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.'
+  description: 'A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.',
+  icons: {
+    icon: '/assets/images/site-logo.svg'
+  }
 }
 
 export default function RootLayout({
