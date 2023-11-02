9a817fae93c26882ecaa68dfb8d295eda7611b7b
Alek Novkovski <alek@greatcode2.com>
2023-11-02 17:04:35 +0100

    Import and enable inter, grotesk fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,18 @@
 import React from "react";
 import { ClerkProvider } from '@clerk/nextjs'
+import { Inter, Space_Grotesk } from 'next/font/google'
 
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  variable: '--font-inter'
+})
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
+  variable: '--font-spaceGrotesk'
+})
 
 export default function RootLayout({
  children,
@@ -10,9 +22,9 @@ export default function RootLayout({
  return (
    <ClerkProvider>
      <html lang="en">
-       <body>{children}</body>
-
-
+       <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+       {children}
+       </body>
      </html>
    </ClerkProvider>
  )
