2777e255ed70bd7e32046c74eda1791b5bdf764b
Alek Novkovski <alek@greatcode2.com>
2023-11-03 17:32:53 +0100

    Add logo and site-name to navbar

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -1,8 +1,21 @@
+import Link from "next/link";
+import Image from 'next/image'
+
 export default function Navbar() {
     return (
         <nav
             className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
-            Navbar
+            <Link href="/" className="flex items-center gap-1">
+                <Image
+                    src="/assets/images/site-logo.svg"
+                    width={23}
+                    height={23}
+                    alt="DevFlow"
+                />
+
+                <p className="h2-bold font-spaceGrotesk text-dark-100 dark:text-light-900 max-sm:hidden">Dev <span
+                    className="text-primary-500">Overflow</span></p>
+            </Link>
         </nav>
     );
 }
