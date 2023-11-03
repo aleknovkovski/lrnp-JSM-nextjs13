488dfdb8c2eabf4e72c9ada95cc2bfd5b86000fc
Alek Novkovski <alek@greatcode2.com>
2023-11-03 17:03:05 +0100

    Fix tailwind: import globals in app-layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { ClerkProvider } from "@clerk/nextjs";
 // eslint-disable-next-line camelcase
 import { Inter, Space_Grotesk } from "next/font/google";
 import { Metadata } from "next";
+
+import './globals.css'
 import {ThemeProvider} from "@/context/ThemeProvider";
 
 const inter = Inter({
