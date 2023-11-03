b4c2c88fada4a9199137c9c74367e014ebf51ac2
Alek Novkovski <alek@greatcode2.com>
2023-11-03 17:11:47 +0100

    Reformat

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -1,7 +1,8 @@
 export default function Navbar() {
-   return (
-       <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
-          Navbar
-       </nav>
-   );
+    return (
+        <nav
+            className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
+            Navbar
+        </nav>
+    );
 }
