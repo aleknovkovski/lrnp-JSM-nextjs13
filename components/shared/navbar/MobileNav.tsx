00f9e291ad97944688762ea8817a24f76ab32f53
Alek Novkovski <alek@greatcode2.com>
2023-11-04 21:04:08 +0100

    Show Signup-Login buttons to nonlogged users

--- components/shared/navbar/MobileNav.tsx
+++ components/shared/navbar/MobileNav.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
-import { SignedOut } from '@clerk/nextjs'
-import { Button } from '@/components/ui/button'
+import {SignedOut} from '@clerk/nextjs'
+import {Button} from '@/components/ui/button'
 
 import {
     Sheet,
@@ -38,6 +38,28 @@ export default function MobileNav() {
                     <SheetClose>
                         NavContent
                     </SheetClose>
+
+                    <SignedOut>
+                        <div className="flex flex-col gap-3">
+                            <SheetClose asChild>
+                                <Link href="/sign-in">
+                                    <Button
+                                        className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
+                                        <span className="primary-text-gradient">Log In</span>
+                                    </Button>
+                                </Link>
+                            </SheetClose>
+
+                            <SheetClose asChild>
+                                <Link href="/sign-up">
+                                    <Button
+                                        className='small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full rounded-lg border px-4 py-3 shadow-none'>
+                                        Sign Up
+                                    </Button>
+                                </Link>
+                            </SheetClose>
+                        </div>
+                    </SignedOut>
                 </div>
             </SheetContent>
         </Sheet>
