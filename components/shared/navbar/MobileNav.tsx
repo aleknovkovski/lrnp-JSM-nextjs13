32781c058d60c8380f9abcbad6308a877a65f3f5
Alek Novkovski <alek@greatcode2.com>
2023-11-04 23:57:31 +0100

    Ascertain if a sidebarLink matches pathname

--- components/shared/navbar/MobileNav.tsx
+++ components/shared/navbar/MobileNav.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import Image from "next/image";
 import {SignedOut} from '@clerk/nextjs'
 import {Button} from '@/components/ui/button'
@@ -10,12 +12,16 @@ import {
 } from "@/components/ui/sheet"
 import Link from "next/link";
 import {sidebarLinks} from "@/constants";
+import {usePathname} from "next/navigation";
 
 const NavContent = () => {
+  const pathname = usePathname();
 
   return (
     <section className="flex h-full flex-col gap-6 pt-16">
       {sidebarLinks.map((item) => {
+          const isActive = (pathname.includes(item.route) && item.route.length > 1) || pathname === item.route;
+
           return (
           <SheetClose asChild key={item.route}>
             <Link
