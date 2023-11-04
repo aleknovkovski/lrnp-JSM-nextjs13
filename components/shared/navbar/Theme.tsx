a6432a4a63fa89a341ca0271cedb609707ab37b7
Alek Novkovski <alek@greatcode2.com>
2023-11-04 15:05:11 +0100

    Style MenubarTrigger

--- components/shared/navbar/Theme.tsx
+++ components/shared/navbar/Theme.tsx
@@ -1,23 +1,38 @@
 'use client'
 import React from "react";
+import Image from 'next/image';
 import {useTheme} from "@/context/ThemeProvider";
 import {
-  Menubar,
-  MenubarContent,
-  MenubarItem,
-  MenubarMenu,
-  MenubarSeparator,
-  MenubarShortcut,
-  MenubarTrigger,
+    Menubar,
+    MenubarContent,
+    MenubarItem,
+    MenubarMenu,
+    MenubarSeparator,
+    MenubarShortcut,
+    MenubarTrigger,
 } from "@/components/ui/menubar"
 
 
 export default function Theme() {
     const {mode, setMode} = useTheme()
     return (
-        <Menubar>
+        <Menubar className="relative border-none bg-transparent shadow-none">
             <MenubarMenu>
-                <MenubarTrigger>File</MenubarTrigger>
+                <MenubarTrigger className="focus:bg-light-900 data-[state=open]:bg-light-900 dark:focus:bg-dark-200 dark:data-[state=open]:bg-dark-200">
+                    {mode === 'light' ? (
+                        <Image
+                            src="/assets/icons/sun.svg" alt="sun"
+                            width={20}
+                            height={20} className="active-theme"
+                        />
+                    ) : (
+                        <Image
+                            src="/assets/icons/moon.svg" alt="moon"
+                            width={20}
+                            height={20} className="active-theme"
+                        />
+                    )}
+                </MenubarTrigger>
                 <MenubarContent>
                     <MenubarItem>
                         New Tab <MenubarShortcut>⌘T</MenubarShortcut>
