adbcd2d219e0e01e052fee350e280abac91032e9
Alek Novkovski <alek@greatcode2.com>
2023-11-04 14:40:02 +0100

    Install shadcn menubar, use example jsx in Theme.tsx

--- components/shared/navbar/Theme.tsx
+++ components/shared/navbar/Theme.tsx
@@ -1,10 +1,34 @@
 'use client'
 import React from "react";
 import {useTheme} from "@/context/ThemeProvider";
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarSeparator,
+  MenubarShortcut,
+  MenubarTrigger,
+} from "@/components/ui/menubar"
 
-export default function Theme () {
+
+export default function Theme() {
     const {mode, setMode} = useTheme()
     return (
-        <>Theme </>
+        <Menubar>
+            <MenubarMenu>
+                <MenubarTrigger>File</MenubarTrigger>
+                <MenubarContent>
+                    <MenubarItem>
+                        New Tab <MenubarShortcut>⌘T</MenubarShortcut>
+                    </MenubarItem>
+                    <MenubarItem>New Window</MenubarItem>
+                    <MenubarSeparator/>
+                    <MenubarItem>Share</MenubarItem>
+                    <MenubarSeparator/>
+                    <MenubarItem>Print</MenubarItem>
+                </MenubarContent>
+            </MenubarMenu>
+        </Menubar>
     );
 }
