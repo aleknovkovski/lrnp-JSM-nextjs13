ae9c7964cf85c3f0d99988dda8be0b5f39e61394
Alek Novkovski <alek@greatcode2.com>
2023-11-05 16:14:08 +0100

    Add Image in Sidebar Links

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,8 +1,27 @@
+import {sidebarLinks} from "@/constants";
+import Link from "next/link";
+import Image from "next/image";
+
 export default function LeftSidebar() {
     return (
-        <nav className="h-screen bg-amber-500 pt-36 lg:w-[266px]">
-
-            LeftSidebar
+        <nav className="h-screen overflow-y-auto bg-amber-500 pt-36 lg:w-[266px]">
+            {sidebarLinks.map((item) => {
+                return (
+                    <Link
+                        key={item.label}
+                        href={item.route}
+                        className="flex items-center justify-start gap-4 bg-transparent p-4"
+                    >
+                        <Image
+                            src={item.imgURL}
+                            alt={item.label}
+                            width={20}
+                            height={20}
+                        />
+                        <p>{item.label}</p>
+                    </Link>
+                )
+            })}
         </nav>
     );
 }
