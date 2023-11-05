83050739d34f2516c4ac7e9028a09d153eb780b1
Alek Novkovski <alek@greatcode2.com>
2023-11-05 21:53:12 +0100

    Change to right-0

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -2,7 +2,7 @@ export default function LeftSidebar() {
 
     return (
         <nav
-            className="custom-scrollbar background-light900_dark200 light-border sticky left-0 top-0 h-screen overflow-y-auto border-r p-6 pt-36 shadow-light-300 dark:shadow-none lg:w-[266px]">
+            className="custom-scrollbar background-light900_dark200 light-border sticky right-0 top-0 h-screen overflow-y-auto border-r p-6 pt-36 shadow-light-300 dark:shadow-none lg:w-[266px]">
 
         </nav>
     );
