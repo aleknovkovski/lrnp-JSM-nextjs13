0febd2eeff5ca14a3103e7e25b67e616fcb39cf7
Alek Novkovski <alek@greatcode2.com>
2023-11-05 21:13:43 +0100

    Add SignedOut buttons

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -3,6 +3,8 @@ import {sidebarLinks} from "@/constants";
 import Link from "next/link";
 import Image from "next/image";
 import {usePathname} from "next/navigation";
+import { SignedOut } from "@clerk/nextjs";
+import { Button } from "../ui/button";
 
 export default function LeftSidebar() {
     const pathname = usePathname();
@@ -35,6 +37,39 @@ export default function LeftSidebar() {
                     )
                 })}
             </div>
+
+            <SignedOut>
+                <div className="flex flex-col gap-3">
+                    <Link href="/sign-in">
+                        <Button
+                            className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
+                            <Image
+                                src="/assets/icons/account.svg"
+                                alt="login"
+                                width={20}
+                                height={20}
+                                className="invert-colors lg:hidden"
+                            />
+                            <span className="primary-text-gradient max-lg:hidden">Log In</span>
+                        </Button>
+                    </Link>
+
+
+                    <Link href="/sign-up">
+                        <Button
+                            className='small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full rounded-lg border px-4 py-3 shadow-none'>
+                            <Image
+                                src="/assets/icons/sign-up.svg"
+                                alt="sign up"
+                                width={20}
+                                height={20}
+                                className="invert-colors lg:hidden"
+                            />
+                            <span className="max-lg:hidden">Sign up</span>
+                        </Button>
+                    </Link>
+                </div>
+            </SignedOut>
         </nav>
     );
 }
