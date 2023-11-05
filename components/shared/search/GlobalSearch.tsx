7904436d623a48d11a50b5913c4c4472cf502779
Alek Novkovski <alek@greatcode2.com>
2023-11-05 13:00:05 +0100

    Add searchicon as Image before Input, and wrap the two in a div

--- components/shared/search/GlobalSearch.tsx
+++ components/shared/search/GlobalSearch.tsx
@@ -1,11 +1,21 @@
+import Image from 'next/image'
 import {Input} from "@/components/ui/input";
 
 export default function GlobalSearch() {
     return (
-        <Input
-            type="text"
-            placeholder="Search globally"
-            value=""
-        />
+       <div>
+       <Image
+           src="/assets/icons/search.svg"
+           alt="search"
+           width={24}
+           height={24}
+           className="cursor-pointer"
+       />
+       <Input
+           type="text"
+           placeholder="Search globally"
+           value=""
+       />
+       </div>
     );
 }
