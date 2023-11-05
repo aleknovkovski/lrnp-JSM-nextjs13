d3908f432a03d49eb7a2535872b1622c5fe8d601
Alek Novkovski <alek@greatcode2.com>
2023-11-05 23:09:14 +0100

    Replace variant with custom styling

--- components/shared/RenderTag.tsx
+++ components/shared/RenderTag.tsx
@@ -8,7 +8,7 @@ interface Props {
 export default function RenderTag(props: Props) {
     return (
         <Link href={`/tags/${props.name}`}>
-            <Badge variant="outline">
+            <Badge className="subtle-medium background-light800_dark300 text-light400_light500 rounded-md border-none px-4 py-2 uppercase">
                 {props.name}
             </Badge>
         </Link>
