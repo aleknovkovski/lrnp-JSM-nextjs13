2022abed0331eda2084bf6c336be79215c77f5c5
Alek Novkovski <alek@greatcode2.com>
2023-11-05 23:31:38 +0100

    Use _id for link, show number of questions for tag

--- components/shared/RenderTag.tsx
+++ components/shared/RenderTag.tsx
@@ -9,10 +9,12 @@ interface Props {
 
 export default function RenderTag(props: Props) {
     return (
-        <Link href={`/tags/${props.name}`}>
+        <Link href={`/tags/${props._id}`}>
             <Badge className="subtle-medium background-light800_dark300 text-light400_light500 rounded-md border-none px-4 py-2 uppercase">
                 {props.name}
             </Badge>
+
+            <p className="small-medium text-dark500_light700">{props.totalQuestions}</p>
         </Link>
     );
 }
