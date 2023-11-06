c339ead1d27d76863760d4ba42b2b2008ce01ddc
Alek Novkovski <alek@greatcode2.com>
2023-11-06 21:08:32 +0100

    Add support for additional classes

--- components/shared/search/LocalSearchbar.tsx
+++ components/shared/search/LocalSearchbar.tsx
@@ -2,12 +2,13 @@ import Image from 'next/image'
 import {Input} from "@/components/ui/input";
 
 interface Props {
+    additionalClasses?: string;
     placeholderText: string
 }
 
 export default function LocalSearchbar(props: Props) {
     return (
-       <div className="background-light800_darkgradient flex min-h-[56px] grow items-center gap-4 rounded-[10px] px-4">
+       <div className={`background-light800_darkgradient flex min-h-[56px] grow items-center gap-4 rounded-[10px] px-4 ${props.additionalClasses}`}>
        <Image
            src="/assets/icons/search.svg"
            alt="search"
