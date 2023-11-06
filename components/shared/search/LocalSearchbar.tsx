f2feabc8221dc741def6b3119cbadcb21481cd94
Alek Novkovski <alek@greatcode2.com>
2023-11-06 21:19:37 +0100

    Set icon img via props

--- components/shared/search/LocalSearchbar.tsx
+++ components/shared/search/LocalSearchbar.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import {Input} from "@/components/ui/input";
 
 interface Props {
+    imgSrc: string;
     additionalClasses?: string;
     placeholderText: string
 }
@@ -10,7 +11,7 @@ export default function LocalSearchbar(props: Props) {
     return (
        <div className={`background-light800_darkgradient flex min-h-[56px] grow items-center gap-4 rounded-[10px] px-4 ${props.additionalClasses}`}>
        <Image
-           src="/assets/icons/search.svg"
+           src={props.imgSrc}
            alt="search"
            width={24}
            height={24}
