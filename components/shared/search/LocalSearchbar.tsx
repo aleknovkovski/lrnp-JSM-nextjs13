097232677872016f77994ba1cd460a120b2942cd
Alek Novkovski <alek@greatcode2.com>
2023-11-06 21:28:42 +0100

    Implement iconPosition setting

--- components/shared/search/LocalSearchbar.tsx
+++ components/shared/search/LocalSearchbar.tsx
@@ -9,22 +9,33 @@ interface Props {
     iconPosition: string;
 }
 
-export default function LocalSearchbar(props: Props) {
+function iconMarkup(imgSrc: string) {
     return (
-       <div className={`background-light800_darkgradient flex min-h-[56px] grow items-center gap-4 rounded-[10px] px-4 ${props.additionalClasses}`}>
-       <Image
-           src={props.imgSrc}
+        <Image
+           src={imgSrc}
            alt="search"
            width={24}
            height={24}
            className="cursor-pointer"
        />
+    )
+}
+
+export default function LocalSearchbar(props: Props) {
+    return (
+       <div className={`background-light800_darkgradient flex min-h-[56px] grow items-center gap-4 rounded-[10px] px-4 ${props.additionalClasses}`}>
+
+       {props.iconPosition === "left" && iconMarkup(props.imgSrc)}
+
        <Input
            type="text"
            placeholder={props.placeholderText}
            value=""
            className="paragraph-regular no-focus placeholder background-light800_darkgradient border-none shadow-none outline-none"
        />
+
+       {props.iconPosition === "right" && iconMarkup(props.imgSrc)}
+
        </div>
     );
 }
