a029295909dce73e1c5ce6815eb6f4432f24e9d5
Alek Novkovski <alek@greatcode2.com>
2023-11-07 14:11:18 +0100

    Hide on desktop

--- components/shared/Filter.tsx
+++ components/shared/Filter.tsx
@@ -15,6 +15,7 @@ interface Props {
 
 export default function Filter(props: Props) {
     return (
+        <div className="hidden max-md:flex">
         <Select>
             <SelectTrigger className={`body-regular light-border background-light800_dark300 text-dark500_light700 border px-5 py-2.5`}>
                 <div className="line-clamp-1 flex-1 text-left">
@@ -29,5 +30,6 @@ export default function Filter(props: Props) {
                 })}
             </SelectContent>
         </Select>
+        </div>
     );
 }
