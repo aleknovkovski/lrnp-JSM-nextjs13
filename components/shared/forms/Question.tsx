b39c375006a1c7c249fff3204b11d040d6e45d60
Alek Novkovski <alek@greatcode2.com>
2023-11-08 19:23:52 +0100

    Add Zod Schema and react-hooks-form example code from shadcn docs

--- components/shared/forms/Question.tsx
+++ components/shared/forms/Question.tsx
@@ -1,7 +1,32 @@
 'use client'
 
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import * as z from "zod"
+
+const formSchema = z.object({
+ username: z.string().min(2, {
+   message: "Username must be at least 2 characters.",
+ }),
+})
+
 export default function Question() {
+   const form = useForm<z.infer<typeof formSchema>>({
+       resolver: zodResolver(formSchema),
+       defaultValues: {
+           username: "",
+       },
+   })
+
+   // 2. Define a submit handler.
+   function onSubmit(values: z.infer<typeof formSchema>) {
+       // Do something with the form values.
+       // ✅ This will be type-safe and validated.
+       console.log(values)
+   }
+
    return (
        <>Question</>
    );
 }
+
