7ecef6b5e3b9b62a52f219010fb48c6d644d4c66
Alek Novkovski <alek@greatcode2.com>
2023-11-08 22:51:04 +0100

    Create Questions Schema for this project

--- lib/validations.ts
+++ lib/validations.ts
@@ -1,7 +1,7 @@
 import * as z from "zod"
 
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
+export const QuestionsSchema = z.object({
+  title: z.string().min(5).max(130),
+  explanation: z.string().min(100),
+  tags: z.array(z.string().min(1).max(15)).min(1).max(3),
 })
