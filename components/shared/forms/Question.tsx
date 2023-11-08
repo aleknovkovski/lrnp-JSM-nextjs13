db45eac821ca3d76d657325e33a429908c371827
Alek Novkovski <alek@greatcode2.com>
2023-11-08 23:46:35 +0100

    Clone first FormField twice, will modify for other inputs

--- components/shared/forms/Question.tsx
+++ components/shared/forms/Question.tsx
@@ -56,6 +56,46 @@ export default function Question() {
                         </FormItem>
                     )}
                 />
+
+                <FormField
+                    control={form.control}
+                    name="title"
+                    render={({field}) => (
+                        <FormItem className="flex w-full flex-col">
+                            <FormLabel className="paragraph-semibold text-dark400_light800">Question Title <span className="text-primary-500">*</span></FormLabel>
+                            <FormControl className="mt-3.5">
+                                <Input
+                                    className="no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border"
+                                    {...field}
+                                />
+                            </FormControl>
+                            <FormDescription className="body-regular mt-2.5 text-light-500">
+                                Be specific and imagine you&apos;re asking a question to another person.
+                            </FormDescription>
+                            <FormMessage className="text-red-500" />
+                        </FormItem>
+                    )}
+                />
+
+                <FormField
+                    control={form.control}
+                    name="title"
+                    render={({field}) => (
+                        <FormItem className="flex w-full flex-col">
+                            <FormLabel className="paragraph-semibold text-dark400_light800">Question Title <span className="text-primary-500">*</span></FormLabel>
+                            <FormControl className="mt-3.5">
+                                <Input
+                                    className="no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border"
+                                    {...field}
+                                />
+                            </FormControl>
+                            <FormDescription className="body-regular mt-2.5 text-light-500">
+                                Be specific and imagine you&apos;re asking a question to another person.
+                            </FormDescription>
+                            <FormMessage className="text-red-500" />
+                        </FormItem>
+                    )}
+                />
                 <Button type="submit">Submit</Button>
             </form>
         </Form>
