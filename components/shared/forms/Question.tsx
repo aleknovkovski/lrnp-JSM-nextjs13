8def61d81d1aa6fcc7908c840d2293d6a1a4d319
Alek Novkovski <alek@greatcode2.com>
2023-11-08 22:09:30 +0100

    Move example schema to validations.ts

--- components/shared/forms/Question.tsx
+++ components/shared/forms/Question.tsx
@@ -16,12 +16,6 @@ import {
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 
-const formSchema = z.object({
- username: z.string().min(2, {
-   message: "Username must be at least 2 characters.",
- }),
-})
-
 export default function Question() {
    const form = useForm<z.infer<typeof formSchema>>({
        resolver: zodResolver(formSchema),
