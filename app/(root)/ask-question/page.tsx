1ab9495d9b02382d76dd43836df5f03d7a0908bb
Alek Novkovski <alek@greatcode2.com>
2023-11-08 18:07:51 +0100

    Create ask-question page

---
+++ app/(root)/ask-question/page.tsx
@@ -0,0 +1,14 @@
+export default async function Page () {
+
+
+ return (
+   <div>
+     <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
+
+
+     <div className="mt-9">
+       Question
+     </div>
+   </div>
+ )
+}
