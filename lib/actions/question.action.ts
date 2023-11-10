b6ac64c2cdb8590fa33dc2ad812785fd83b4f42c
Alek Novkovski <alek@greatcode2.com>
2023-11-10 01:36:56 +0100

    Setup example server action structure

---
+++ lib/actions/question.action.ts
@@ -0,0 +1,9 @@
+"use server"
+
+export async function createQuestion(params: any) {
+    try {
+        console.log('connecting to database')
+    } catch (error) {
+        console.log('error')
+    }
+}
