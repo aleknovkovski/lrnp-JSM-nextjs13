52bd9b9afc5963adca76b212904f237a5315a5be
Alek Novkovski <alek@greatcode2.com>
2023-11-11 00:28:05 +0100

    destructure received params

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -6,6 +6,7 @@ export async function createQuestion(params: any) {
     try {
         await connectToDatabase()
         console.log('connected to database')
+        const { title, content, tags, author, path } = params;
     } catch (error) {
         console.log('error')
     }
