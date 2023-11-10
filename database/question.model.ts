c7609a9ebcdc3b467a3597c1b99d1ed9e985b552
Alek Novkovski <alek@greatcode2.com>
2023-11-10 16:43:51 +0100

    Define interface for question documents structure typing

---
+++ database/question.model.ts
@@ -0,0 +1,13 @@
+import { Schema, models, model, Document } from 'mongoose';
+
+export interface IQuestion extends Document {
+  title: string;
+  content: string;
+  tags: Schema.Types.ObjectId[];
+  views: number;
+  upvotes: Schema.Types.ObjectId[];
+  downvotes: Schema.Types.ObjectId[];
+  author: Schema.Types.ObjectId;
+  answers: Schema.Types.ObjectId[];
+  createdAt: Date;
+}
