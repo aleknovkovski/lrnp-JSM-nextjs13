ade6279b98b91b094dcb04dd5771fc9cd03c8f62
Alek Novkovski <alek@greatcode2.com>
2023-11-11 19:33:50 +0100

    Reformat code

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,17 +1,17 @@
 import Question from "@/components/shared/forms/Question";
 
 
-export default async function Page () {
+export default async function Page() {
 
 
- return (
-   <div>
-     <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
+    return (
+        <div>
+            <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
 
 
-     <div className="mt-9">
-       <Question />
-     </div>
-   </div>
- )
+            <div className="mt-9">
+                <Question/>
+            </div>
+        </div>
+    )
 }
