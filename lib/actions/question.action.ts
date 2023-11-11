2bf658b14de9d08d31a299884f5becd3be953b13
Alek Novkovski <alek@greatcode2.com>
2023-11-11 16:13:13 +0100

    Create a Question in MongoDB

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -1,12 +1,21 @@
 "use server"
 
 import {connectToDatabase} from "@/lib/mongoose";
+import Question from "@/database/question.model";
 
 export async function createQuestion(params: any) {
     try {
         await connectToDatabase()
         console.log('connected to database')
         const { title, content, tags, author, path } = params;
+
+        // Create the question
+        const question = await Question.create({
+            title,
+            content,
+            author
+        });
+
     } catch (error) {
         console.log('error')
     }
