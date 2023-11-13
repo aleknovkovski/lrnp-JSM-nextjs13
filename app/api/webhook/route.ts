4ec074df23d078b5b83bf364b92a84966b5e906d
Alek Novkovski <alek@greatcode2.com>
2023-11-13 13:56:34 +0100

    Create updateUser action, and use it in webhook

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -2,7 +2,7 @@
 import { Webhook } from 'svix'
 import { headers } from 'next/headers'
 import { WebhookEvent } from '@clerk/nextjs/server'
-import {createUser} from "@/lib/actions/user.action";
+import {createUser, updateUser} from "@/lib/actions/user.action";
 import {NextResponse} from "next/server";
 
 export async function POST(req: Request) {
@@ -75,7 +75,7 @@ export async function POST(req: Request) {
   if(eventType === 'user.updated') {
     const { id, email_addresses, image_url, username, first_name, last_name } = evt.data;
 
-    // Create a new user in your database
+    // Update a user in your database
     const mongoUser = await updateUser({
       clerkId: id,
       updateData: {
