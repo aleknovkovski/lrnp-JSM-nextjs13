ab532699a8decfa1ea972b71dc794e12c05a8aaf
Alek Novkovski <alek@greatcode2.com>
2023-11-16 22:00:39 +0100

    Render Question's tags below the question

--- app/(root)/question/[id]/page.tsx
+++ app/(root)/question/[id]/page.tsx
@@ -4,6 +4,7 @@ import Metric from "@/components/shared/Metric";
 import Image from 'next/image';
 import Link from 'next/link';
 import ParseHTML from "@/components/shared/ParseHTML";
+import RenderTag from "@/components/shared/RenderTag";
 
 interface params {params: { id: string }}
 
@@ -62,6 +63,17 @@ export default async function QuestionPage({params} : params) {
       </div>
 
       <ParseHTML data={result.content} />
+
+      <div className="mt-8 flex flex-wrap gap-2">
+        {result.tags.map((tag: any) => (
+          <RenderTag
+            key={tag._id}
+            _id={tag._id}
+            name={tag.name}
+            showCount={false}
+          />
+        ))}
+      </div>
     </>
   )
 }
