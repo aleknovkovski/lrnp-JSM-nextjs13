293ed281d8e75b6fab7540c8601aa2d49928d723
Alek Novkovski <alek@greatcode2.com>
2023-11-16 14:12:43 +0100

    Create getQuestionById fn, and use on page to show question title

--- app/(root)/question/[id]/page.tsx
+++ app/(root)/question/[id]/page.tsx
@@ -1,7 +1,12 @@
+import {getQuestionById} from "@/lib/actions/question.action";
+
 interface params {params: { id: string }}
 
-export default function QuestionPage({params} : params) {
+export default async function QuestionPage({params} : params) {
+
+    const question = await getQuestionById({questionId: params.id})
+
     return (
-        <>{`QuestionPage for question ${params.id}`}</>
+        <>{`QuestionPage for question "${question.title}"`}</>
     );
 }
--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -23,6 +23,21 @@ export async function getQuestions(params: GetQuestionsParams) {
   }
 }
 
+export async function getQuestionById(params: any) {
+  try {
+    connectToDatabase();
+
+    const { questionId } = params;
+
+    const question = await Question.findOne({ _id: questionId });
+
+    return question;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
 export async function createQuestion(params: CreateQuestionParams) {
     try {
         await connectToDatabase()
