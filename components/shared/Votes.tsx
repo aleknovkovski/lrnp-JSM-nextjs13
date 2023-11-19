0e8f541a0604fe685c10098e45a15eabace63d75
Alek Novkovski <alek@greatcode2.com>
2023-11-19 21:15:19 +0100

    Create the Votes layout

--- components/shared/Votes.tsx
+++ components/shared/Votes.tsx
@@ -1,3 +1,8 @@
+'use client'
+
+import Image from "next/image";
+import {formatAndDivideNumber} from "@/lib/utils";
+
 interface Props {
   type: string;
   itemId: string;
@@ -19,7 +24,72 @@ export default function Votes ({
   hasdownVoted,
   hasSaved,
 }: Props) {
+
+  const handleSave = async () => {
+    console.log("handleSave ran")
+  }
+
+  const handleVote = async (action: string) => {
+    console.log("handleVote ran")
+    }
+
   return (
-    <div>Votes</div>
+    <div className="flex gap-5">
+      <div className="flex-center gap-2.5">
+        <div className="flex-center gap-1.5">
+          <Image
+            src={hasupVoted
+              ? '/assets/icons/upvoted.svg'
+              : '/assets/icons/upvote.svg'
+            }
+            width={18}
+            height={18}
+            alt="upvote"
+            className="cursor-pointer"
+            onClick={() => handleVote('upvote')}
+          />
+
+          <div className="flex-center background-light700_dark400 min-w-[18px] rounded-sm p-1">
+            <p className="subtle-medium text-dark400_light900">
+              {formatAndDivideNumber(upvotes)}
+            </p>
+          </div>
+        </div>
+
+        <div className="flex-center gap-1.5">
+          <Image
+            src={hasdownVoted
+              ? '/assets/icons/downvoted.svg'
+              : '/assets/icons/downvote.svg'
+            }
+            width={18}
+            height={18}
+            alt="downvote"
+            className="cursor-pointer"
+            onClick={() => handleVote('downvote')}
+          />
+
+          <div className="flex-center background-light700_dark400 min-w-[18px] rounded-sm p-1">
+            <p className="subtle-medium text-dark400_light900">
+              {formatAndDivideNumber(downvotes)}
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {type === 'Question' && (
+        <Image
+          src={hasSaved
+            ? '/assets/icons/star-filled.svg'
+            : '/assets/icons/star-red.svg'
+          }
+          width={18}
+          height={18}
+          alt="star"
+          className="cursor-pointer"
+          onClick={handleSave}
+        />
+      )}
+    </div>
   )
 }
