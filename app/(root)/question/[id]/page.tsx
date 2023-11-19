dca6497e4525e55dfd966ee545b8a21d8743821b
Alek Novkovski <alek@greatcode2.com>
2023-11-19 21:09:12 +0100

    Create votes component, define props, call from question details page

--- app/(root)/question/[id]/page.tsx
+++ app/(root)/question/[id]/page.tsx
@@ -9,6 +9,7 @@ import Answer from "@/components/forms/Answer";
 import {auth} from "@clerk/nextjs";
 import {getUserById} from "@/lib/actions/user.action";
 import AllAnswers from "@/components/shared/AllAnswers";
+import Votes from "@/components/shared/Votes";
 
 interface params {params: { id: string }}
 
@@ -41,7 +42,16 @@ export default async function QuestionPage({params} : params) {
             </p>
           </Link>
           <div className="flex justify-end">
-            VOTING
+            <Votes
+              type="Question"
+              itemId={JSON.stringify(result._id)}
+              userId={JSON.stringify(mongoUser._id)}
+              upvotes={result.upvotes.length}
+              hasupVoted={result.upvotes.includes(mongoUser._id)}
+              downvotes={result.downvotes.length}
+              hasdownVoted={result.downvotes.includes(mongoUser._id)}
+              hasSaved={mongoUser?.saved.includes(result._id)}
+            />
           </div>
         </div>
         <h2 className="h2-semibold text-dark200_light900 mt-3.5 w-full text-left">
---
+++ components/shared/Votes.tsx
@@ -0,0 +1,25 @@
+interface Props {
+  type: string;
+  itemId: string;
+  userId: string;
+  upvotes: number;
+  hasupVoted: boolean;
+  downvotes: number;
+  hasdownVoted: boolean;
+  hasSaved?: boolean;
+}
+
+export default function Votes ({
+  type,
+  itemId,
+  userId,
+  upvotes,
+  hasupVoted,
+  downvotes,
+  hasdownVoted,
+  hasSaved,
+}: Props) {
+  return (
+    <div>Votes</div>
+  )
+}
