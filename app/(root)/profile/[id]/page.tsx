4f9b2a58ef6a8ba2374be00b711372b91684fe1f
Alek Novkovski <alek@greatcode2.com>
2023-12-03 16:07:23 +0100

    Implement AnswersTab on profile page

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -9,6 +9,7 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import ProfileLink from "@/components/shared/ProfileLink";
 import Stats from '@/components/shared/Stats'
 import QuestionTab from "@/components/shared/QuestionTab";
+import AnswersTab from '@/components/shared/AnswersTab'
 
 const Page = async ({ params, searchParams}: URLProps) => {
   const { userId: clerkId } = auth();
@@ -92,7 +93,11 @@ const Page = async ({ params, searchParams}: URLProps) => {
               />
           </TabsContent>
           <TabsContent value="answers" className="flex w-full flex-col gap-6">
-            Answers
+              <AnswersTab
+                  searchParams={searchParams}
+                  userId={userInfo.user._id}
+                  clerkId={clerkId}
+              />
           </TabsContent>
         </Tabs>
       </div>
