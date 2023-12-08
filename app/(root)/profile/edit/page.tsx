1a005dd8053ad2106b61ae18aa88b1a4cfa144a5
Alek Novkovski <alek@greatcode2.com>
2023-12-08 22:25:28 +0100

    Implement Profile form on profile edit page

--- app/(root)/profile/edit/page.tsx
+++ app/(root)/profile/edit/page.tsx
@@ -1,12 +1,24 @@
+import { getUserById } from '@/lib/actions/user.action';
 import { ParamsProps } from '@/types';
+import { auth } from '@clerk/nextjs'
+import Profile from "@/components/forms/Profile";
 
 export default async function Page ({ params }: ParamsProps){
-        return (
+    const { userId } = auth();
+
+    if(!userId) return null;
+
+    const mongoUser = await getUserById({ userId })
+
+    return (
         <>
             <h1 className="h1-bold text-dark100_light900">Edit Profile</h1>
 
             <div className="mt-9">
-                Insert profile form here
+                <Profile
+                    clerkId={userId}
+                    user={JSON.stringify(mongoUser)}
+                />
             </div>
         </>
     )
