6c9dcf2770bb43e92fde0c46db63e34d73f2108a
Alek Novkovski <alek@greatcode2.com>
2023-12-08 22:09:17 +0100

    Create edit profile page

---
+++ app/(root)/profile/edit/page.tsx
@@ -0,0 +1,13 @@
+import { ParamsProps } from '@/types';
+
+export default async function Page ({ params }: ParamsProps){
+        return (
+        <>
+            <h1 className="h1-bold text-dark100_light900">Edit Profile</h1>
+
+            <div className="mt-9">
+                Insert profile form here
+            </div>
+        </>
+    )
+}
