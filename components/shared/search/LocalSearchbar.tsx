393e95e31b0c40afebe6f7d9b85feb73820a3f08
Alek Novkovski <alek@greatcode2.com>
2023-12-12 22:32:31 +0100

    Add searched-for-value to url query string

--- components/shared/search/LocalSearchbar.tsx
+++ components/shared/search/LocalSearchbar.tsx
@@ -2,8 +2,9 @@
 
 import Image from 'next/image'
 import {Input} from "@/components/ui/input";
-import {useSearchParams} from "next/navigation";
-import {useState} from "react";
+import {usePathname, useRouter, useSearchParams} from "next/navigation";
+import {useEffect, useState} from "react";
+import {formUrlQuery} from "@/lib/utils";
 
 interface Props {
     imgSrc: string;
@@ -26,11 +27,30 @@ function iconMarkup(imgSrc: string) {
 }
 
 export default function LocalSearchbar(props: Props) {
+    const router = useRouter();
+    const pathname = usePathname();
     const searchParams = useSearchParams();
 
     const query = searchParams.get('q');
 
     const [search, setSearch] = useState(query || '');
+
+    useEffect(() => {
+        const delayDebounceFn = setTimeout(() => {
+            if(search) {
+                const newUrl = formUrlQuery({
+                    params: searchParams.toString(),
+                    key: 'q',
+                    value: search
+                })
+
+                router.push(newUrl, { scroll: false });
+            }
+
+        }, 300);
+
+        return () => clearTimeout(delayDebounceFn)
+    }, [search, props.route, pathname, router, searchParams, query])
 
     return (
        <div className={`background-light800_darkgradient flex min-h-[56px] grow items-center gap-4 rounded-[10px] px-4 ${props.additionalClasses}`}>
