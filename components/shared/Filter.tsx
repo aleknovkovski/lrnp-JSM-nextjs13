7900ceb76b8b94c42717f1d2e70122ae17e22ed2
Alek Novkovski <alek@greatcode2.com>
2023-12-14 21:19:54 +0100

    Change filter param in url based on selection

--- components/shared/Filter.tsx
+++ components/shared/Filter.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import {
     Select,
     SelectContent,
@@ -6,6 +8,8 @@ import {
     SelectTrigger,
     SelectValue,
 } from "@/components/ui/select"
+import {useRouter, useSearchParams} from "next/navigation";
+import {formUrlQuery} from "@/lib/utils";
 
 interface Props {
   filters: {
@@ -17,9 +21,27 @@ interface Props {
 }
 
 export default function Filter(props: Props) {
+    const searchParams = useSearchParams();
+    const router = useRouter();
+
+    const paramFilter = searchParams.get('filter');
+
+    const handleUpdateParams = (value: string) => {
+        const newUrl = formUrlQuery({
+            params: searchParams.toString(),
+            key: 'filter',
+            value
+        })
+
+        router.push(newUrl, { scroll: false })
+    }
+
     return (
         <div className={`relative ${props.containerClasses}`}>
-        <Select>
+        <Select
+            onValueChange={handleUpdateParams}
+            defaultValue={paramFilter || undefined}
+        >
             <SelectTrigger className={`${props.additionalClasses} body-regular light-border background-light800_dark300 text-dark500_light700 border px-5 py-2.5`}>
                 <div className="line-clamp-1 flex-1 text-left">
                     <SelectValue placeholder="Select a Filter" />
