21f66e2000094be62e8f020a8259a5ec165b289c
Alek Novkovski <alek@greatcode2.com>
2023-12-15 21:50:49 +0100

    Utilize Pagination on HomePage

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -8,11 +8,13 @@ import NoResult from "@/components/shared/NoResult";
 import QuestionCard from "@/components/cards/QuestionCard";
 import {getQuestions} from "@/lib/actions/question.action";
 import {SearchParamsProps} from "@/types";
+import Pagination from "@/components/shared/Pagination";
 
 export default async function Home({ searchParams }: SearchParamsProps) {
     const results = await getQuestions({
         searchQuery: searchParams.q,
         filter: searchParams.filter,
+        page: searchParams.page ? +searchParams.page : 1
     });
 
     return (
@@ -66,6 +68,12 @@ export default async function Home({ searchParams }: SearchParamsProps) {
                         link="/ask-question"
                         linkTitle="Ask a Question"
                     />}
+                    <div className="mt-10">
+                        <Pagination
+                            pageNumber={searchParams?.page ? +searchParams.page : 1}
+                            hasNext={results.hasNext}
+                        />
+                    </div>
             </div>
         </>
     )
