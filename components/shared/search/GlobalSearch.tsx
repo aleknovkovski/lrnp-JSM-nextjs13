4aa2c48054af180d510d78b7cfc7c1efebc42b70
Alek Novkovski <alek@greatcode2.com>
2023-12-18 19:36:26 +0100

    Implement debounce and two-way binding

--- components/shared/search/GlobalSearch.tsx
+++ components/shared/search/GlobalSearch.tsx
@@ -1,22 +1,65 @@
+"use client"
+
 import Image from 'next/image'
 import {Input} from "@/components/ui/input";
+import {formUrlQuery, removeKeysFromQuery} from '@/lib/utils';
+import {useEffect, useState} from "react";
+import {usePathname, useRouter, useSearchParams} from "next/navigation";
 
 export default function GlobalSearch() {
+
+    const router = useRouter();
+    const pathname = usePathname();
+    const searchParams = useSearchParams();
+
+    const query = searchParams.get('q');
+
+    const [search, setSearch] = useState(query || '');
+
+    useEffect(() => {
+        const delayDebounceFn = setTimeout(() => {
+            if(search) {
+                const newUrl = formUrlQuery({
+                    params: searchParams.toString(),
+                    key: 'global',
+                    value: search
+                })
+
+                router.push(newUrl, { scroll: false });
+            } else {
+                if(query) {
+                    const newUrl = removeKeysFromQuery({
+                        params: searchParams.toString(),
+                        keysToRemove: ['global', 'type']
+                    })
+
+                    router.push(newUrl, { scroll: false });
+                }
+
+            }
+        }, 300);
+
+        return () => clearTimeout(delayDebounceFn)
+    }, [search, router, pathname, searchParams, query])
+
     return (
-       <div className="background-light800_darkgradient relative flex min-h-[56px] grow items-center gap-1 rounded-xl px-4">
-       <Image
-           src="/assets/icons/search.svg"
-           alt="search"
-           width={24}
-           height={24}
-           className="cursor-pointer"
-       />
-       <Input
-           type="text"
-           placeholder="Search globally"
-           defaultValue=""
-           className="paragraph-regular no-focus placeholder background-light800_darkgradient border-none shadow-none outline-none"
-       />
-       </div>
+        <div className="background-light800_darkgradient relative flex min-h-[56px] grow items-center gap-1 rounded-xl px-4">
+            <Image
+                src="/assets/icons/search.svg"
+                alt="search"
+                width={24}
+                height={24}
+                className="cursor-pointer"
+            />
+            <Input
+                type="text"
+                placeholder="Search globally"
+                value={search}
+                onChange={(e) => {
+                    setSearch(e.target.value);
+                }}
+                className="paragraph-regular no-focus placeholder text-dark400_light700 background-light800_darkgradient border-none shadow-none outline-none"
+            />
+        </div>
     );
 }
