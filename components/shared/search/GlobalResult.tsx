e7aef26ad9a691f0cc6f8f91a5aef0731e13c444
Alek Novkovski <alek@greatcode2.com>
2023-12-20 13:00:28 +0100

    Implement GlobalFilters

--- components/shared/search/GlobalResult.tsx
+++ components/shared/search/GlobalResult.tsx
@@ -5,6 +5,7 @@ import { ReloadIcon } from '@radix-ui/react-icons'
 import { useSearchParams } from 'next/navigation'
 import Link from 'next/link'
 import Image from 'next/image'
+import GlobalFilters from "@/components/shared/search/GlobalFilters";
 
 
 const GlobalResult = () => {
@@ -58,7 +59,7 @@ const GlobalResult = () => {
 
     return (
         <div className="absolute top-full z-10 mt-3 w-full rounded-xl bg-light-800 py-5 shadow-sm dark:bg-dark-400">
-            {"<GlobalFilters />"}
+            <GlobalFilters />
             <div className="my-5 h-[1px] bg-light-700/50 dark:bg-dark-500/50" />
 
             <div className="space-y-5">
