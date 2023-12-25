0c9091248913cb5dcebe24bfc97edcd63a031ce9
Alek Novkovski <alek@greatcode2.com>
2023-12-25 16:14:57 +0100

    Place chatgpt reply in editor

--- app/api/chatgpt/route.ts
+++ app/api/chatgpt/route.ts
@@ -30,7 +30,6 @@ export const POST = async (request: Request) => {
         const responseData = await response.json();
         const reply = responseData.choices[0].message.content;
 
-
         return NextResponse.json({ reply })
     } catch (error: any) {
         return NextResponse.json({ error: error.message })
--- components/forms/Answer.tsx
+++ components/forms/Answer.tsx
@@ -73,6 +73,15 @@ const Answer = ({ question, questionId, authorId }: Props) => {
 
             const aiAnswer = await response.json();
 
+            // Convert plain text to HTML format
+
+            const formattedAnswer = aiAnswer.reply.replace(/\n/g, '<br />');
+
+            if(editorRef.current) {
+                const editor = editorRef.current as any;
+                editor.setContent(formattedAnswer);
+            }
+
         } catch (error) {
             console.log(error);
         } finally {
